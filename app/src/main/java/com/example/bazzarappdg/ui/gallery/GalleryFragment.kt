0136841a0aacb312db29f422ef6a5010619239f8
Daniel H Garcia <danielhgarciab@gmail.com>
2021-12-03 15:17:33 -0500

    DG2021120301 UI Modificación descripción producto DG2021120302 Carrito de compras

--- app/src/main/java/com/example/bazzarappdg/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/bazzarappdg/ui/gallery/GalleryFragment.kt
@@ -1,5 +1,6 @@
 package com.example.bazzarappdg.ui.gallery
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,38 +9,88 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.bazzarappdg.R
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bazzarappdg.*
 import com.example.bazzarappdg.databinding.FragmentGalleryBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.launch
 
 class GalleryFragment : Fragment() {
 
     private lateinit var galleryViewModel: GalleryViewModel
     private var _binding: FragmentGalleryBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var recycleView: RecyclerView
+    private var db = FirebaseFirestore.getInstance()
+    private var listCar = mutableListOf<CarEntity>()
+    private lateinit var carAdapter: CarAdapter
+    private var total = 0
+    private var totalText :TextView?=null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        galleryViewModel = ViewModelProvider(this).get(GalleryViewModel::class.java)
 
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        //Recycler
+        recycleView = root.findViewById<RecyclerView>(R.id.reclyclerCar)
+        recycleView.layoutManager = LinearLayoutManager(activity)
+        recycleView.setHasFixedSize(true)
+
+        getAllCarNew()
+        carAdapter = CarAdapter(listCar);
+        recycleView.adapter = carAdapter;
+
+        totalText = root.findViewById<TextView>(R.id.total)
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun getAllCarNew() {
+        listCar.clear()
+
+        var idCar = ""
+        val prefs = requireActivity().getSharedPreferences(resources.getString(R.string.preds_file), Context.MODE_PRIVATE)
+        idCar = prefs.getString("car", null).toString()
+
+        if(idCar != null && idCar.isNotEmpty()){
+
+            db.collection("car").document(idCar).collection("products")
+                .get().addOnSuccessListener {
+                    if (it.any()) {
+                        for (item in it) {
+                            listCar.add(
+                                CarEntity(
+                                    item.data["image"].toString(),
+                                    item.data["title"].toString(),
+                                    item.data["cost"].toString(),
+                                    item.data["amount"].toString(),
+                                    item.id.toString()
+                                )
+                            )
+
+                            total += (item.data["cost"].toString()
+                                .toInt() * item.data["amount"].toString().toInt())
+
+                            totalText!!.text = total.toString()
+                        }
+                        carAdapter.notifyDataSetChanged();
+                    }
+                }
+
+        }
+
     }
 }
